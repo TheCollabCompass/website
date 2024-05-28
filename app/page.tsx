975d86742ce62099de8cdef138eab1c884fa13a4
Rana Ahsan Ansar <142870716+AhsanVaivalBlockchain@users.noreply.github.com>
2024-05-28 13:45:28 +0500

    add Pakages Includes and Plab Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,49 @@
-import Image from "next/image";
-import MainBar from "./Components/MainNav";
-import { Button } from "@nextui-org/react";
-import LiveTripCard from "./Components/LiveTripCard";
+import Image from 'next/image';
+import MainBar from './Components/MainNav';
+import { Button } from '@nextui-org/react';
+import LiveTripCard from './Components/LiveTripCard';
 
 export default function Home() {
   return (
     <>
-      <section id="hero-section" className="backgroundImage">
+      <section id='hero-section' className='backgroundImage'>
         {/* <MainBar /> */}
 
-        <div className="md:container md:mx-auto hero-container ">
-          <div className="p-2">
-            <div className="grid md:grid-cols-2 sm:grid-cols-1"> 
-              <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-gray-200 rounded-lg shadow-lg p-8">
-              <div>
-                <div className="text-4xl font-bold tracking-wider">
-                Explore Pakistan with Your Favorite Influencers!
+        <div className='md:container md:mx-auto hero-container '>
+          <div className='p-2'>
+            <div className='grid md:grid-cols-2 sm:grid-cols-1'>
+              <div className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-gray-200 rounded-lg shadow-lg p-8'>
+                <div>
+                  <div className='text-4xl font-bold tracking-wider'>
+                    Explore Pakistan with Your Favorite Influencers!
+                  </div>
+
+                  <p className='text-2xl mt-4 tracking-wider font-semibold'>
+                    Discover the majestic beauty of Pakistan's mountain regions with the influencers you follow and
+                    love!
+                  </p>
+                  <p className='text-1xl mt-2 tracking-wider'>Pakistan's First Influencer-Led Tourism Company!</p>
                 </div>
 
-                <p className="text-2xl mt-4 tracking-wider font-semibold">
-                Discover the majestic beauty of Pakistan's mountain regions with the influencers you follow and love!
-                </p>
-                <p className="text-1xl mt-2 tracking-wider">
-                Pakistan's First Influencer-Led Tourism Company!
-                </p>
-              </div>
-
-              <div className="mt-2">
-                <Button color="success" variant="shadow">
-                  Explore
-                </Button>
+                <div className='mt-2'>
+                  <Button color='success' variant='shadow'>
+                    Explore
+                  </Button>
+                </div>
               </div>
             </div>
-            </div>
-            
           </div>
         </div>
       </section>
 
-      <section id="explore-section">
-        <div className="container mx-auto">
-          <div className="py-20 px-2">
-            <div className="flex flex-col space-y-24">
-              <div className="flex flex-row items-center justify-center">
-                <h2 className="text-4xl font-bold">Comming Trips</h2>
+      <section id='explore-section'>
+        <div className='container mx-auto'>
+          <div className='py-20 px-2'>
+            <div className='flex flex-col space-y-24'>
+              <div className='flex flex-row items-center justify-center'>
+                <h2 className='my-8 text-4xl font-bold px-2 underline decoration-white'>Comming Trips</h2>
               </div>
-              <div className="flex flex-row items-center  justify-around flex-wrap">
+              <div className='flex flex-row items-center  justify-around flex-wrap'>
                 <div>
                   <LiveTripCard />
                 </div>
--- app/trip-info/page.tsx
+++ app/trip-info/page.tsx
@@ -1,20 +1,89 @@
-import React from "react";
-import LiveTripCard from "../Components/LiveTripCard";
-import TripInfoTripCard from "../Components/TripInfoTripCard";
-import TripInfoCollaboratorCard from "../Components/TripInfoCollaboratorCard";
+import React from 'react';
+import LiveTripCard from '../Components/LiveTripCard';
+import TripInfoTripCard from '../Components/TripInfoTripCard';
+import TripInfoCollaboratorCard from '../Components/TripInfoCollaboratorCard';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 
 const page = () => {
   return (
     <>
-      <section className="py-10">
-        <div className="container mx-auto h-full" >
-            <div className="h-full flex items-center justify-center m-4" >
-                <div  className="w-full flex flex-wrap justify-around">
-                <div><TripInfoTripCard /></div>
-                <div><TripInfoCollaboratorCard /></div>
-
-                </div>
+      <section className='py-10'>
+        <div className='container mx-auto h-full'>
+          <div className='h-full flex items-center justify-center m-4'>
+            <div className='w-full flex flex-wrap justify-around'>
+              <div>
+                <TripInfoTripCard />
+              </div>
+              <div>
+                <TripInfoCollaboratorCard />
+              </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section id='package-includes-section'>
+        <div className='container mx-auto h-full'>
+          <div className='flex flex-wrap flex-col justify-center items-center'>
+            <div className='my-8 text-4xl font-bold px-2 underline decoration-white'>Package Includes</div>
+            <div id='packages-container' className='p-4 w-full'>
+              <div className='flex flex-wrap flex-row w-full'>
+                <div className='grid md:grid-cols-2 sm:grid-cols-1 gap-4 w-full'>
+                  <div className='flex flex-row items-center justify-center mb-2'>
+                    <DirectionsBusIcon style={{ fontSize: '5rem', color: 'white' }} className='mr-2' />
+                    <div className='text-2xl font-semibold '>Luxury Transport</div>
+                  </div>
+                  <div className='flex flex-row items-center justify-center mb-2'>
+                    <DirectionsBusIcon style={{ fontSize: '5rem', color: 'white' }} className='mr-2' />
+                    <div className='text-2xl font-semibold '>Luxury Transport</div>
+                  </div>
+                  <div className='flex flex-row items-center justify-center mb-2'>
+                    <DirectionsBusIcon style={{ fontSize: '5rem', color: 'white' }} className='mr-2' />
+                    <div className='text-2xl font-semibold '>Luxury Transport</div>
+                  </div>
+                  <div className='flex flex-row items-center justify-center mb-2'>
+                    <DirectionsBusIcon style={{ fontSize: '5rem', color: 'white' }} className='mr-2' />
+                    <div className='text-2xl font-semibold '>Luxury Transport</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id='plan-section'>
+        <div className='container mx-auto h-full'>
+          <div className='flex flex-wrap flex-col justify-center items-center'>
+            <div className='my-8 text-4xl font-bold px-2 underline decoration-white'>Trip Plan</div>
+            <div id='plan-container' className='p-4 w-full'>
+              <div className='flex flex-wrap flex-row w-full timeline-container'>
+                <div className='mb-4  '>
+                  <div className='text-2xl font-semibold'>Day : 0</div>
+                  <p className='p-2 bg-white bg-opacity-10 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 overflow-hidden'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, reprehenderit! Cumque, ducimus
+                    architecto aspernatur nam ea labore assumenda natus provident, veniam perspiciatis tempore vero
+                    accusamus, incidunt quisquam iusto quos placeat?
+                  </p>
+                </div>
+                <div className='mb-4'>
+                  <div className='text-2xl font-semibold mb-2'>Day : 1</div>
+                  <p className='p-2 bg-white bg-opacity-10 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 overflow-hidden'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, reprehenderit! Cumque, ducimus
+                    architecto aspernatur nam ea labore assumenda natus provident, veniam perspiciatis tempore vero
+                    accusamus, incidunt quisquam iusto quos placeat?
+                  </p>
+                </div>
+                <div className='mb-4'>
+                  <div className='text-2xl font-semibold'>Day : 2</div>
+                  <p className='p-2 bg-white bg-opacity-10 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 overflow-hidden'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, reprehenderit! Cumque, ducimus
+                    architecto aspernatur nam ea labore assumenda natus provident, veniam perspiciatis tempore vero
+                    accusamus, incidunt quisquam iusto quos placeat?
+                  </p>
+                </div>
+                <div className='timeline-line'></div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
